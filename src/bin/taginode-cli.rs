b98bb2fd91deb6af2c4646d9ea1f733cd1c2597a
wangerda <wangerda@outlook.com>
2022-10-13 00:19:10 +0800

    opt

--- src/bin/taginode-cli.rs
+++ src/bin/taginode-cli.rs
@@ -4,44 +4,76 @@ use std::fs;
 use std::io::{Error, ErrorKind};
 use std::os::unix::prelude::MetadataExt;
 use std::time::UNIX_EPOCH;
+use sqlite3::Connection;
 use taginode::INode;
 
 fn usage() {
-    eprintln!("Usage: taginode-cli tag <file> [tag1 tag2...]");
-    eprintln!("Usage: taginode-cli search [tag1 tag2...]");
-    eprintln!("Usage: taginode-cli list tags");
+    eprintln!("Usage: taginode-cli [option] tag <file> <tag> [tag1,tag2...]");
+    eprintln!("Usage: taginode-cli [option] search [tag1,tag2...]");
+    eprintln!("Usage: taginode-cli [option] list tags");
+    eprintln!(
+"Option: 
+        -f <db>
+                Specify db path to store data, default ~/.taginode.db
+"   );
     std::process::exit(1);
 }
 
-fn main() -> std::io::Result<()>{
+fn main() -> Result<(), Error>{
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    let args = &args[1..];
+
+    let mut options: HashMap<u8, &str> = HashMap::new();
+    let mut operands: Vec<&str> = Vec::new();
+    let mut i = 0;
+    while i < args.len() {
+        let arg = args[i].as_str();
+        match arg {
+            "-f" => {
+                if i+1 >= args.len() {
+                    eprint!("Option -f need option-argument");
+                    usage();
+                }
+                options.insert(b'f', args[i+1].as_str());
+                i += 1;
+            },
+            "--" => {
+                for e in &args[(i+1)..] {
+                    operands.push(e);
+                }
+                break;
+            }
+            other => operands.push(other),
+        }
+        i += 1;
+    } 
+    if operands.is_empty() {
         usage();
     }
-    match args[1].as_str() {
-        "tag" => tag(),
-        "search" => search(),
-        "list" => list(),
+
+    let mut h = env::var("HOME").unwrap();
+    h.push_str("/.taginode.db");
+    let default_db_path = h.as_str();
+    let db_path = options.get(&b'f').unwrap_or(&default_db_path);
+    let db = taginode::sql::init(&db_path);
+
+    match operands[0] {
+        "tag" => tag(&operands[1..], db),
+        "search" => search(&operands[1..], db),
+        "list" => list(&operands[1..], db),
         _ => usage(),
     }
     Ok(())
 }
 
-fn tag() {
-    let args: Vec<String> = env::args().collect();
-    let args: Vec<&str> = args[2..].iter().map(|val| {
-        val.as_str()
-    }).collect();
+fn tag(args: &[&str], db: Connection) {
     if args.len() < 2 {
         usage();
     }
     let files = &args[0..1];
-    let tag_names = &args[1..];
+    let tag_names: Vec<&str> = args[1].split(",").collect();
     eprintln!("tag_names: {:?}, files: {:?}", tag_names, files);
 
-    let mut db_file = env::var("HOME").unwrap();
-    db_file.push_str("/.taginode.db");
-    let connection = taginode::sql::init(&db_file);
     for file in files {
         let metadata = fs::metadata(file.to_string());
         let metadata = match metadata {
@@ -67,29 +99,22 @@ fn tag() {
                 None
             }
         };
-        taginode::add(&connection, 
+        taginode::add(&db, 
             &vec![ INode{ device: metadata.dev(), number: metadata.ino(), btime } ],
             &tag_names,
         );
     }
 }
 
-fn search() {
-    let args: Vec<String> = env::args().collect();
-    let args: Vec<&str> = args[2..].iter().map(|val| {
-        val.as_str()
-    }).collect();
-    if args.len() < 1 {
+fn search(args: &[&str], db: Connection) {
+    if args.len() != 1 {
         usage();
     }
-    let tag_names = &args[0..];
+    let tag_names: Vec<&str> = args[0].split(",").collect();
     let paths = vec!["."];
     eprintln!("tag_names: {:?}, paths: {:?}", tag_names, paths);
 
-    let mut db_file = env::var("HOME").unwrap();
-    db_file.push_str("/.taginode.db");
-    let connection = taginode::sql::init(&db_file);
-    let inodes = taginode::get_inodes(&connection, tag_names);
+    let inodes = taginode::get_inodes(&db, &tag_names);
     let mut dev_inode_map: HashMap<u64, HashMap<u64, &INode>> = HashMap::new();
     for inode in &inodes {
         let inode_map = dev_inode_map.get_mut(&inode.device);
@@ -159,18 +184,11 @@ fn process_file(dev_inode_map: &HashMap<u64, HashMap<u64, &INode>>, f: &str) -> 
     Ok(())
 }
 
-fn list() {
-    let args: Vec<String> = env::args().collect();
-    let args: Vec<&str> = args[2..].iter().map(|val| {
-        val.as_str()
-    }).collect();
+fn list(args: &[&str], db: Connection) {
     if args.len() < 1 && args[0] != "tags" {
         usage();
     }
 
-    let mut db_file = env::var("HOME").unwrap();
-    db_file.push_str("/.taginode.db");
-    let connection = taginode::sql::init(&db_file);
-    let tag_names = taginode::list_tags(&connection);
+    let tag_names = taginode::list_tags(&db);
     println!("{tag_names:?}")
 }

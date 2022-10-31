7b26d7d9a5fc28ceb66eb4f92e470615c8a453ac
wangerda <wangerda@outlook.com>
2022-11-01 00:53:39 +0800

    add option -a -u

--- src/bin/taginode-cli.rs
+++ src/bin/taginode-cli.rs
@@ -28,9 +28,14 @@ fn main() -> Result<(), Error>{
     default_db.push_str("/.taginode.db");
 
     let opt_check = BTreeMap::from([
-        (b'f', (OptArg::Mandatory(default_db.as_str()), "-f <db>        specify db path to store data, default ~/.taginode.db"  )),
-        (b'd', (                OptArg::Mandatory("."), "-d <directory> specify path to search file by tags, default \".\""     )),
-        (b'v', (                          OptArg::None, "-v             verbose"                                                )),
+        (b'f', (OptArg::Mandatory(default_db.as_str()), "-f <db>        specify db path to store data, default ~/.taginode.db"                           )),
+        (b'd', (                OptArg::Mandatory("."), "-d <directory> [search]specify path to search file by tags, default \".\""                      )),
+        (b'a', (                          OptArg::None, "-a             [search]ensable cross devices, default only search dev of path specified by -d"  )),
+        (b'u', (                          OptArg::None, "-u             [search]output same inode(default remove duplicate item"                         )),
+        // (b'v', (                          OptArg::None, "-v             verbose"                                                                    )),
+        // (b'V', (                          OptArg::None, "-V             version"                                                                    )),
+        // (b'l', (                          OptArg::None, "-l             follow symbolic links instead of symbolic file itself"                      )),
+        // (b'5', (                          OptArg::None, "-5             md5 mode instead of inode"                                                  )),
     ]);
     let usage = usage(&opt_check);
 
@@ -71,7 +76,7 @@ fn tag(operands: &[&str], db: Connection) -> Result<(), Error> {
     eprintln!("tag_names: {:?}, files: {:?}", tag_names, files);
 
     for file in files {
-        let metadata = fs::metadata(file.to_string());
+        let metadata = fs::symlink_metadata(file.to_string());
         let metadata = match metadata {
             Ok(metadata) => metadata,
             Err(error) => {
@@ -126,57 +131,75 @@ fn search(operands: &[&str], options: HashMap<u8, &str>, db: Connection) -> Resu
             }
         };
     }
+
+    let mut occur: Option<HashMap<u64, HashMap<u64, String>>> = match options.get(&b'u') {
+        Some(_) => None,
+        None => Some(HashMap::new()),
+    };
     for path in paths {
-        match process_file(&dev_inode_map, path) {
-            Err(error) => eprintln!("{error:?}"),
+        match process_file(&dev_inode_map, path, options.get(&b'a').is_some(), &mut occur) {
+            Err(error) => eprintln!("{path}: {error:?}"),
             _ => (),
         }
     }
     Ok(())
 }
 
-fn process_file(dev_inode_map: &HashMap<u64, HashMap<u64, &INode>>, f: &str) -> Result<(), Error> {
-    let metadata = fs::metadata(f)?;
-    if dev_inode_map.get(&metadata.dev()).is_some() {
-        let inode_map = dev_inode_map.get(&metadata.dev()).unwrap();
-        let mut hit = false;
+fn process_file(dev_inode_map: &HashMap<u64, HashMap<u64, &INode>>, f: &str, cross_dev: bool, occur: &mut Option<HashMap<u64, HashMap<u64, String>>>) -> Result<(), Error> {
+    let metadata = fs::symlink_metadata(f)?;
+    if occur.is_some() {
+        let occur = occur.as_mut().unwrap();
+        match occur.get_mut(&metadata.dev()) {
+            Some(s) => {
+                match s.get_mut(&metadata.ino()) {
+                    Some(old) => {
+                        eprintln!("{}: same file as '{}'",f, old);
+                        return Ok(())
+                    }, 
+                    None => {
+                        s.insert(metadata.ino(), f.to_string());
+                    },
+                }
+            },
+            None => {
+                let mut inode_map = HashMap::new();
+                inode_map.insert(metadata.ino(), f.to_string());
+                occur.insert(metadata.dev(), inode_map);
+            },
+        }
+    }
+    match dev_inode_map.get(&metadata.dev()) {
+        Some(inode_map) => {
         match inode_map.get(&metadata.ino()) {
             None => (),
             Some(ino) => {
-                let created = (|| -> Result<u64, Error> {
-                    match metadata.created()?.duration_since(UNIX_EPOCH) {
-                        Ok(btime) => 
-                        Ok(btime.as_secs()),
-                        Err(error) => 
-                        Err(Error::new(ErrorKind::Other, error.to_string())),
-                    }
-                })();
-                if ino.btime == None || created.is_err() || ino.btime.unwrap() == created.unwrap() {
-                    hit = true;
+                let created = get_file_btime(metadata.created());
+                if ino.btime == None || created.is_none() || ino.btime.unwrap() == created.unwrap() {
+                    println!("{} ", f);
                 }
             }
         };
-        if hit {
-            println!("{} ", f);
+        },
+        None if !cross_dev => return Ok(()), 
+        _ => (),
+    }
+    if metadata.is_dir() {
+        if metadata.is_symlink() {
+            return Ok(())
         }
-        if metadata.is_dir() {
-            let paths = fs::read_dir(f)?;
-            for path in paths {
-                match path {
-                    Ok(entry) => {
-                        if entry.metadata()?.is_symlink() {
-                            continue;
-                        }
-                        let p = entry.path();
-                        let p= p.to_str().unwrap();
-                        match process_file(&dev_inode_map, p) {
-                            Err(error) => eprintln!("{p:?} {error:?}"),
-                            _ => (),
-                        }
+        let paths = fs::read_dir(f)?;
+        for path in paths {
+            match path {
+                Ok(entry) => {
+                    let p = entry.path();
+                    let p= p.to_str().unwrap();
+                    match process_file(&dev_inode_map, p, cross_dev, occur) {
+                        Err(error) => eprintln!("{p}: {error:?}"),
+                        _ => (),
                     }
-                    Err(error) => eprintln!("{f} {error:?}"),
-                };
-            }
+                }
+                Err(error) => eprintln!("{f} {error:?}"),
+            };
         }
     }
     Ok(())
@@ -199,7 +222,7 @@ fn cat(args: &[&str], db: Connection) -> Result<(), Error> {
         return err_str("");
     }
     for path in args {
-        let metadata = fs::metadata(path);
+        let metadata = fs::symlink_metadata(path);
         print!("{}:    ", path);
         match metadata {
             Ok(metadata) => {
